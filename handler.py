33c0ea67637b3628e8f89b805e79a3e6045aad87
macgroigar <stephenmacgregor4@gmail.com>
2022-08-29 08:32:13 +0100

    create image function yml settings

--- handler.py
+++ handler.py
@@ -1,4 +1,6 @@
+from contextlib import nullcontext
 from datetime import datetime
+from urllib import response
 import boto3
 from io import BytesIO
 from PIL import Image, ImageOps
@@ -6,8 +8,59 @@ import os
 import uuid
 import json
 
+s3 = boto3.client('s3')
+size = int(os.environ['THUMBNAIL_SIZE'])
 
-def hello(event, context):
+def s3_thumbnail_generator(event, context):
+    #parse event
+    print("EVENT:::", event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    img_size = event['Records'][0]['s3']['object']['size']
+
+    if (not key.endswith("_thumbnail.png")):
+        image = get_s3_image(bucket, key)
+
+        thumbnail = image_to_thumbnail(image)
+        thumbnail_key = new_filename(key)
+
+        url = upload_to_s3(bucket, thumbnail_key, thumbnail, img_size)
+
+        return url
+
+def get_s3_image(bucket, key):
+    response = s3.get_object(BUCKET=bucket, Key=key)
+    imagecontent = response['body'].read()
+
+    file = BytesIO(imagecontent)
+    img = Image.open(file)
+    return img
+
+def image_to_thumbnail(image):
+    return ImageOps.fit(image, (size, size), Image.ANTIALIAS)
+
+def new_filename(key):
+    key_split = key.rsplit('.', 1)
+    return key_split[0] + "_thumbnail.png"
+
+def upload_to_s3(bucket, key, image, img_size):
+    out_thumbnail = BytesIO
+    image.save(out_thumbnail, 'PNG')
+    out_thumbnail.seek(0)
+
+    response = s3.put_object(
+        ACL='public-read',
+        Body=out_thumbnail,
+        Bucket=bucket,
+        ContentType='image/png',
+        Key=key
+    )
+    print(response)
+
+    url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
+
+    return url
+
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
